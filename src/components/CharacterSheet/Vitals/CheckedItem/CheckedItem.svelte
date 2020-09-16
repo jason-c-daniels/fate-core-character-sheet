6af7159b32fe57b57400dba20750c6771059758f
Jason Daniels <jason.c.daniels@gmail.com>
2020-09-16 16:02:20 -0500

    * implemented stress tracks

---
+++ src/components/CharacterSheet/Vitals/CheckedItem/CheckedItem.svelte
@@ -0,0 +1,42 @@
+<style>
+    @import "CheckedItem.css";
+</style>
+<script context="module">
+    export const defaultCss = {
+        color: "black",
+        backgroundColor: "transparent",
+        size: "1.45em",
+        borderWidth: "1.0pt"
+    };
+</script>
+<script>
+    import {createEventDispatcher} from 'svelte'
+    import Check from './small-check.svg';
+    export let number = 1;
+    export let css = defaultCss;
+    export let checked;
+    const dispatch = createEventDispatcher();
+
+    function handleClick() {
+        checked = !checked;
+        dispatch('checkedChanged', { checked: checked });
+    }
+</script>
+
+<div
+        class="box"
+        style="margin-left:0.5rem;margin-right:1.5rem;color:{css.color}; width:{css.size}; height:{css.size}; border-width:{css.borderWidth};
+  border-color:{css.color}; border-style:solid; background-color:{css.backgroundColor};" on:click={handleClick}>
+    <div class="check-text" style="width:{css.size}; height:{css.size}; font-size: {css.size}">
+    <span>
+      {number}
+    </span>
+    </div>
+    {#if checked}
+        <div class="checkmark" style="width:100%; height:100%;">
+            <div>
+                <Check width="{css.size}" height="{css.size}"/>
+            </div>
+        </div>
+    {/if}
+</div>
--- src/components/CharacterSheet/Vitals/Vitals.svelte
+++ src/components/CharacterSheet/Vitals/Vitals.svelte
@@ -3,32 +3,45 @@
 </style>
 
 <script>
-    import Section from "../../shared/Section/Section.svelte";
+    import Section from "../../shared/Section";
+    import CheckedItem, {defaultCss as CSS} from "./CheckedItem/CheckedItem.svelte";
     export let vitals={};
+    let css={...CSS,color:'black', size:"1.6rem", margin:"1rem;"};
+    let grayCss={...css,color:'gray'};
 </script>
 
 <div class="flex-row" style="width: 100%">
 <div style="width: 60%; flex-shrink: 2; flex-grow: 3; margin-right:1rem;">
     <Section title="Physical Stress" subtitle="(Physique)">
-        TBD
+        <div class="flex-row" style="height:4rem;">
+            <CheckedItem {css} number="1" bind:checked={vitals.physicalStress[0].taken} />
+            <CheckedItem {css} number="2" bind:checked={vitals.physicalStress[1].taken} />
+            <CheckedItem css={grayCss} number="3" bind:checked={vitals.physicalStress[2].taken} />
+            <CheckedItem css={grayCss} number="4" bind:checked={vitals.physicalStress[3].taken} />
+        </div>
     </Section>
     <Section title="Mental Stress" subtitle="(Will)">
-        TBD
+        <div class="flex-row" >
+            <CheckedItem {css} number="1" bind:checked={vitals.mentalStress[0].taken} />
+            <CheckedItem {css} number="2" bind:checked={vitals.mentalStress[1].taken} />
+            <CheckedItem css={grayCss} number="3" bind:checked={vitals.mentalStress[2].taken} />
+            <CheckedItem css={grayCss} number="4" bind:checked={vitals.mentalStress[3].taken} />
+        </div>
     </Section>
 </div>
     <Section title="Consequences" props={{style:"width:100%; flex-grow:1;flex-shrink:1;height:300px"}}>
         <div class="consequences">
             <div>
-            <label for="mild1">2</label><input id="mild1" type="text" placeholder="Mild" bind:value={vitals.consequences.mild1} />
+            <label for="mild1">2</label><input id="mild1" type="text" placeholder=" Mild" bind:value={vitals.consequences.mild1} />
             </div>
             <div>
-            <label for="moderate">4</label><input id="moderate" type="text" placeholder="Moderate" bind:value={vitals.consequences.moderate} />
+            <label for="moderate">4</label><input id="moderate" type="text" placeholder=" Moderate" bind:value={vitals.consequences.moderate} />
             </div>
             <div>
-            <label for="severe">6</label><input id="severe" type="text" placeholder="Severe" bind:value={vitals.consequences.severe} />
+            <label for="severe">6</label><input id="severe" type="text" placeholder=" Severe" bind:value={vitals.consequences.severe} />
             </div>
             <div>
-            <label for="mild2" style="color:var(--input-border-color); break-before: column">2</label><input id="mild2" type="text" placeholder="Mild" bind:value={vitals.consequences.mild2} />
+            <label for="mild2" style="color:var(--input-border-color); break-before: column">2</label><input id="mild2" type="text" placeholder=" Mild" bind:value={vitals.consequences.mild2} />
             </div>
         </div>
     </Section>
