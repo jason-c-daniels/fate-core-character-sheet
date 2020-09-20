fac1e48d8b6250a60da58471c5a3655e7ec95915
Jason Daniels <jason.c.daniels@gmail.com>
2020-09-19 20:47:53 -0500

    * improved binding by makign a custom textfield.

--- src/App/SettingsDialog/SettingsDialog.svelte
+++ src/App/SettingsDialog/SettingsDialog.svelte
@@ -14,6 +14,7 @@
     import getNewDials from '../../model/dials'
     import {dials} from '../../applicationSettings'
     import Switch from "./Switch/Switch.svelte";
+    import TextField from "./TextField/TextField.svelte";
 
     let my_dials = getNewDials();
 
@@ -45,10 +46,10 @@
     export function getDials() {
         return my_dials;
     }
+
     function handleDialogClosed(evt) {
-        if (evt.detail.action === 'ok'){
-            my_dials.attributesName=attributesNameElement.value;
-            my_dials.middleRows = middleRowsElement.value;
+        if (evt.detail.action === 'ok') {
+
             my_dials.stressBoxesGrayAfter = parseInt(stressBoxesGrayAfterElement.value);
             my_dials.numberOfStressBoxes = parseInt(numberOfStressBoxesElement.value);
             my_dials.physicalStressTitle = physicalStressTitleElement.value;
@@ -68,24 +69,24 @@
         <Switch title="Use skills" bind:value={my_dials.useSkillsInsteadOfApproaches} />
         <Switch title="Use approaches" bind:value={my_dials.useApproachesInsteadOfSkills} />
         <Switch title="Use attributes" bind:value={my_dials.useAttributes} />
-        <mwc-textfield bind:this={attributesNameElement} type="text" label="Attributes Section Name" value={my_dials.attributesName} style="width:100%"/>
+        <TextField type="text" label="Attributes Section Name" bind:value={my_dials.attributesName} style="width:100%"/>
 
         <h3>Equipment, Stunts Extras</h3>
-        <mwc-textfield bind:this={middleRowsElement} type="number" min="5" max="16" label="Number of Rows in Middle Section" value={my_dials.middleRows} style="width:100%"/>
+        <TextField type="number" min="5" max="16" label="Number of Rows in Middle Section" bind:value={my_dials.middleRows} style="width:100%"/>
         <Switch title="Use equipment." bind:value={my_dials.useEquipment} />
         <Switch title="Use extras" bind:value={my_dials.useExtras} />
 
         <h3>Stress</h3>
         <Switch title="Use single point stress boxes." bind:value={my_dials.useSinglePointStressBoxes} />
         <Switch title="Use single stress track" bind:value={my_dials.useSingleStressTrack} />
-        <mwc-textfield bind:this={numberOfStressBoxesElement} type="number" min="1" max="12" label="Number of Stress Boxes" value={my_dials.numberOfStressBoxes} style="width:100%"/>
-        <mwc-textfield bind:this={stressBoxesGrayAfterElement} type="number" min="1" max="12" label="Stress Boxes Become Gray After #" value={my_dials.stressBoxesGrayAfter} style="width:100%"/>
-        <mwc-textfield bind:this={physicalStressTitleElement} type="text" label="Physical stress track title" value={my_dials.physicalStressTitle} style="width:100%"/>
-        <mwc-textfield bind:this={physicalStressSubtitleElement} type="text" label="Physical stress track subtitle" value={my_dials.physicalStressSubtitle} style="width:100%"/>
-        <mwc-textfield bind:this={mentalStressTitleElement} type="text" label="Mental stress title" value={my_dials.mentalStressTitle} style="width:100%"/>
-        <mwc-textfield bind:this={mentalStressSubtitleElement} type="text" label="Mental stress track subtitle" value={my_dials.mentalStressSubtitle} style="width:100%"/>
-        <mwc-textfield bind:this={singleStressTitleElement} type="text" label="Single stress track title" value={my_dials.singleStressTitle} style="width:100%"/>
-        <mwc-textfield bind:this={singleStressSubtitleElement} type="text" label="Single stress track subtitle" value={my_dials.singleStressSubtitle} style="width:100%"/>
+        <TextField type="number" min="1" max="12" label="Number of Stress Boxes" bind:value={my_dials.numberOfStressBoxes} style="width:100%"/>
+        <TextField type="number" min="1" max="12" label="Stress Boxes Become Gray After #" bind:value={my_dials.stressBoxesGrayAfter} style="width:100%"/>
+        <TextField type="text" label="Physical stress track title" enabled={!my_dials.useSingleStressTrack} bind:value={my_dials.physicalStressTitle} style="width:100%"/>
+        <TextField type="text" label="Physical stress track subtitle" enabled={!my_dials.useSingleStressTrack} bind:value={my_dials.physicalStressSubtitle} style="width:100%"/>
+        <TextField type="text" label="Mental stress title" enabled={!my_dials.useSingleStressTrack} bind:value={my_dials.mentalStressTitle} style="width:100%"/>
+        <TextField type="text" label="Mental stress track subtitle" enabled={!my_dials.useSingleStressTrack} bind:value={my_dials.mentalStressSubtitle} style="width:100%"/>
+        <TextField type="text" label="Single stress track title" enabled={my_dials.useSingleStressTrack} bind:value={my_dials.singleStressTitle} style="width:100%"/>
+        <TextField type="text" label="Single stress track subtitle" enabled={my_dials.useSingleStressTrack} bind:value={my_dials.singleStressSubtitle} style="width:100%"/>
 
         <h3>Conditions and Consequences</h3>
         <Switch title="Use conditions" bind:value={my_dials.useConditions}/>
---
+++ src/App/SettingsDialog/TextField/TextField.svelte
@@ -0,0 +1,29 @@
+<style>
+    @import "TextField.css";
+</style>
+
+<script>
+    export let value;
+    let element;
+    export let type="text";
+    export let min="";
+    export let max="";
+    export let label="";
+    export let enabled=true;
+    export let style="";
+    export let className="";
+    $:disabled= enabled ? "" : "disabled";
+</script>
+
+<mwc-textfield
+        bind:this={element}
+        {type}
+        {min}
+        {max}
+        {label}
+        {value}
+        {style}
+        class="{className}"
+        {disabled}
+        on:change={()=> value= (type==='number' ? parseInt(element.value): element.value) }
+        />
